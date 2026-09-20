bbf9f6b0df075258b399137eec02918728d9737d
Rudra Bahadur Karki <karkirudra856@gmail.com>
2026-09-20 23:00:29 +0545

    updated cart reload

--- cart.php
+++ cart.php
@@ -1,6 +1,10 @@
-
 <?php
 require_once 'includes/auth.php';
+
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Expires: 0');
 
 $page_title = 'Cart';
 
@@ -812,4 +816,11 @@ if (savedSelections) {
 
 </script>
 <?php include 'includes/footer.php'; ?>
+<script>
+window.addEventListener("pageshow", function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+});
+</script>
 
